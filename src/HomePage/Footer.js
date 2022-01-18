629249c1c81d24ec493df9c49e2eef488a3b3e3c
Donovan Laws <donovan.laws@gmail.com>
2022-01-18 14:01:39 -0700

    Footer fix

--- src/HomePage/Footer.js
+++ src/HomePage/Footer.js
@@ -2,6 +2,6 @@ import React from "react";
 
 export default function Footer() {
     return (
-        <h2 className="App-header mb-0 pt-2">Footer</h2>
+        <h2 className="App-header mb-0 py-2" style={{fontSize: "20px"}}>Intellectual Property of Donovan Laws</h2>
     );
 }
